03fb4cee2e6581a8ff6f2898ae7dabdb98691129
Felix Roske <f.roske@roskenet.de>
2015-09-20 21:23:08 +0200

    First attempt readImage from S3
    * Not streamed yet

--- src/main/java/de/roskenet/simplestorage/storage/S3Storage.java
+++ src/main/java/de/roskenet/simplestorage/storage/S3Storage.java
@@ -1,36 +1,48 @@
 package de.roskenet.simplestorage.storage;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.context.annotation.Profile;
-
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.util.IOUtils;
 
 @Component
 @Profile("default")
 public class S3Storage implements PersistentStorage {
 
-    @Value("${storage.bucket.name}")
-    private String bucketName;
+	@Value("${storage.bucket.name}")
+	private String bucketName;
 
-    @Autowired
-    private AmazonS3 amazonS3;
+	@Autowired
+	private AmazonS3 amazonS3;
 
-    @Override
-    public void write(final String id, final byte[] bytes) {
+	@Override
+	public void write(final String id, final byte[] bytes) {
 
-        Thread uploadThread = new Thread(new UploadTask(amazonS3, id, bytes, bucketName));
-        uploadThread.start();
+		Thread uploadThread = new Thread(new UploadTask(amazonS3, id, bytes, bucketName));
+		uploadThread.start();
 
-    }
+	}
 
-    @Override
-    public byte[] read(final String id) {
+	@Override
+	public byte[] read(final String id) {
+		S3Object s3object = amazonS3.getObject(new GetObjectRequest(bucketName, id));
 
-        return null;
-    }
+		InputStream stream = s3object.getObjectContent();
+		try {
+			return IOUtils.toByteArray(stream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
