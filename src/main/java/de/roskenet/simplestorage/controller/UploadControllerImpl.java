73f0c8c18751279aa04ec9bd91bacfd1750e00cf
Felix Roske <f.roske@roskenet.de>
2015-09-29 22:10:48 +0200

    Restrict to image/jpeg

--- src/main/java/de/roskenet/simplestorage/controller/UploadControllerImpl.java
+++ src/main/java/de/roskenet/simplestorage/controller/UploadControllerImpl.java
@@ -6,11 +6,18 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,7 +52,11 @@ public class UploadControllerImpl implements UploadController {
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public ResponseEntity<InputStreamResource> download(@PathVariable("id") String id) {
     	
-        return ResponseEntity.ok(storage.read(id));
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.add("Content-Type", "image/jpeg");
+    	ResponseEntity<InputStreamResource> responseEntity = new ResponseEntity<InputStreamResource>(storage.read(id), headers, HttpStatus.OK);
+    	
+    	return responseEntity;
     }
 
     @Override
